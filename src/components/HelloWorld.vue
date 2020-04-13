3b64989b38fbc2ed172b7dd734a716ed2e0ab64e
Juan Rios <juan.rios.28@gmail.com>
2020-04-12 21:51:33 -0400

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div> {{ msg }} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Hello World',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
