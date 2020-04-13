a7551bc6c2c29e9a11f47afa51394c4fcb0bd4aa
Juan Rios <juan.rios.28@gmail.com>
2020-04-12 23:29:35 -0400

    added axios, initial get request

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,44 @@
 <template>
   <div>
-    <div> {{ msg }} </div>
-    <fish-button type="primary">Testing</fish-button>
+    <h1 v-if="loading"> loading... </h1>
+    <div v-if="errors">
+      <h1> here </h1>
+      <h1> {{ errors }} </h1>
+      <h1 v-for="error in errors" :key="error"> {{ error }} </h1>
+    </div>
+    <fish-button type="primary" v-on:click="getPosts">load more</fish-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'HelloWorld',
   data() {
     return {
-      msg: 'Hello World',
+      posts: [],
+      errors: [],
+      loading: false,
     };
+  },
+  methods: {
+    getPosts: () => {
+      this.loading = true;
+      axios.get('https://dev.to/api/articles')
+        .then((response) => {
+          // eslint-disable-next-line no-console
+          console.log(response);
+          this.loading = false;
+          this.posts = response;
+        })
+        .catch((e) => {
+          this.loading = false;
+          this.errors = e;
+        });
+    },
+  },
+  mounted() {
+    return this.getPosts();
   },
 };
 </script>
